c328adabdbddaab8f7a3b0b79cd1aa8de176b3fe
dhruvSuhird <suhiriddhruv218@gmail.com>
2025-06-24 16:43:50 +0530

    Initial Commit

---
+++ NutriPal/NutriPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriPalApp.swift
+//  NutriPal
+//
+//  Created by Dhruv Suhird on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NutriPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
